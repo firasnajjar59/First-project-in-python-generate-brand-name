2b9e19c48c0d96f8b00848224a66153b3fa86e7e
firasnajjar59 <firasnajjar92@gmail.com>
2023-02-11 14:26:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to brand name generator")
+country_name=input("What's name of the city you grow up in?")
+country_name=country_name.capitalize()
+pet_name=input("What'syour pet's name?")
+pet_name=pet_name.capitalize()
+print("Your brand name could be "+country_name+" "+pet_name)
